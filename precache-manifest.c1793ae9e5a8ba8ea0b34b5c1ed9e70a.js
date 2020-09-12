8152b2b52c88b0b8d85c3588281c689c0d7ab43a
bibekgupta3333 <bibekgupta3333@gmail.com>
2020-09-12 21:00:58 +0545

    Updates

--- precache-manifest.c1793ae9e5a8ba8ea0b34b5c1ed9e70a.js
+++ precache-manifest.c1793ae9e5a8ba8ea0b34b5c1ed9e70a.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "79c3ad7177d11378058631fdb2d2d6d0",
+    "revision": "b7a9f91467c5731f3e8af476dd5dbabe",
     "url": "/e-medhub/index.html"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/e-medhub/static/js/2.241b7992.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "78b3b3588221a1811c19",
-    "url": "/e-medhub/static/js/main.175c6199.chunk.js"
+    "revision": "5cc7e462faba9e7eab90",
+    "url": "/e-medhub/static/js/main.c5c8329f.chunk.js"
   },
   {
     "revision": "b46bca48ed9d206e208f",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/e-medhub/precache-manifest.e8c16b4a09ffeb309cc347e3e2db91b5.js"
+  "/e-medhub/precache-manifest.c1793ae9e5a8ba8ea0b34b5c1ed9e70a.js"
 );
 
 self.addEventListener('message', (event) => {
